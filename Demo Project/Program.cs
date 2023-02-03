91e23f6df3289b447f9148dc37cebf06ed3a03be
pawankamdi <pawankamdi26@gmail.com>
2023-02-03 19:00:06 +0530

    INITALSTAGE

---
+++ Demo Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Demo_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please entre the name");
+            Console.ReadLine();
+        }
+    }
+}
